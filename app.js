11e3f3b20814d0b1615e27c7e1beed89895b8051
Pratik Shahi <shahipr0@gmail.com>
2021-03-20 16:32:42 +0530

    bodyparser gets signup detail

--- app.js
+++ app.js
@@ -14,7 +14,13 @@ app.get("/",function(req,res){
     res.sendFile(__dirname+"/signup.html");
 });
 
-
+app.post("/",function(req,res){
+    var firstName=req.body.fname;
+    var lastName=req.body.lname;
+    var email=req.body.email;
+   // res.send("<h1>post received</h1>");
+    console.log(firstName, lastName, email );
+});
 
 
 app.listen(3000,function(){
