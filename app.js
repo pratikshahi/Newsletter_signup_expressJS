e844dfca58b6b8d02b8871d95c1e52dfb695bd07
Pratik Shahi <shahipr0@gmail.com>
2021-03-20 16:11:35 +0530

    local file inclusion with express while ocal host

--- app.js
+++ app.js
@@ -2,14 +2,21 @@
 
 const express=require("express");
 const bodyParser=require("body-parser");
+const request=require("request");
 
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
 
+//for local file to use while local hosting
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+});
 
 
 
 
 app.listen(3000,function(){
     console.log("server is running on port 3000");
-})
+});
